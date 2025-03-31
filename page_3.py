dd5162c9cadc25568fa617a2e7be770f0c5ee227
Will Toth <wtoth1631@gmail.com>
2025-03-30 19:13:40 -0500

    Update intake metrics

--- page_3.py
+++ page_3.py
@@ -6,9 +6,9 @@ st.sidebar.markdown("# Intake Metrics")
 
 con = st.session_state['conn']
 
-df = con.execute("""
--- CREATE MACRO IF NOT EXISTS LINE_TO_POINT(A, B, C, x, y) AS ABS(A*x + B*y + C) / SQRT(POWER(A, 2) + POWER(B,2));
-
+@st.cache_data
+def get_data():
+  return con.execute("""
 -- Drive Station Lines (Standard Form) Ax + By + C = 0
 SET VARIABLE Blue1A = -0.59;
 SET VARIABLE Blue1B = -0.84;
@@ -35,6 +35,9 @@ WITH data_table AS (
   FROM (
     SELECT *,
       ABS(OmegaVelocity) + ABS(XVelocity) + ABS(YVelocity) < 0.01 AS RobotStopped,
+      CASE WHEN PoseX1 IS NULL THEN PoseX2 ELSE PoseX1 END AS PoseX,
+      CASE WHEN PoseY1 IS NULL THEN PoseX2 ELSE PoseY1 END AS PoseY,
+      CASE WHEN PoseTheta1 IS NULL THEN PoseTheta2 ELSE PoseTheta1 END AS PoseTheta,
       LINE_TO_POINT(getvariable('Blue1A'), getvariable('Blue1B'), getvariable('Blue1C'), PoseX, PoseY) As DistanceToBlueLeftFeeder,
       LINE_TO_POINT(getvariable('Blue2A'), getvariable('Blue2B'), getvariable('Blue2C'), PoseX, PoseY) As DistanceToBlueRightFeeder,
       LINE_TO_POINT(getvariable('Red1A'), getvariable('Red1B'), getvariable('Red1C'), PoseX, PoseY) As DistanceToRedLeftFeeder,
@@ -45,21 +48,24 @@ WITH data_table AS (
       IntakeState = 'STOPPED' AND InnerSensorTriggered AS IntakeStoppedWithCoral,
       FROM (
         SELECT loop_count, filename,
-                MIN(timestamp) as timestamp,
-                last_value(Max("/RealOutputs/RobotState/OdometryPose").x IGNORE nulls) OVER (PARTITION BY filename ORDER BY loop_count ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS PoseX,
-                last_value(Max("/RealOutputs/RobotState/OdometryPose").y IGNORE nulls) OVER (PARTITION BY filename ORDER BY loop_count ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS PoseY,
-                last_value(Max("/RealOutputs/RobotState/OdometryPose").value IGNORE nulls) OVER (PARTITION BY filename ORDER BY loop_count ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS PoseTheta,
-                last_value(Max("/RealOutputs/SwerveChassisSpeeds/Measured").vx IGNORE nulls) OVER (PARTITION BY filename ORDER BY loop_count ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS XVelocity,
-                last_value(Max("/RealOutputs/SwerveChassisSpeeds/Measured").vy IGNORE nulls) OVER (PARTITION BY filename ORDER BY loop_count ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS YVelocity,
-                last_value(Max("/RealOutputs/SwerveChassisSpeeds/Measured").omega IGNORE nulls) OVER (PARTITION BY filename ORDER BY loop_count ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS OmegaVelocity,
-                last_value(Max("/DriverStation/Enabled") IGNORE nulls) OVER (PARTITION BY filename ORDER BY loop_count ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS Enabled,
-                last_value(Max("/RealOutputs/Launcher/State") IGNORE nulls) OVER (PARTITION BY filename ORDER BY loop_count ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS IntakeState,
-                last_value(max("/DriverStation/MatchTime") IGNORE nulls) OVER (PARTITION BY filename ORDER BY loop_count ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS MatchTime,
-                last_value(max("/Launcher/Sensor/InnerSensorTriggered") IGNORE nulls) OVER (PARTITION BY filename ORDER BY loop_count ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS InnerSensorTriggered,
-                last_value(max("/DriverStation/MatchNumber") IGNORE nulls) OVER (PARTITION BY filename ORDER BY loop_count ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS MatchNumber,
-                last_value(max("/DriverStation/MatchType") IGNORE nulls) OVER (PARTITION BY filename ORDER BY loop_count ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS MatchType,
-                last_value(max("/DriverStation/EventName") IGNORE nulls) OVER (PARTITION BY filename ORDER BY loop_count ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS EventName,
-        FROM frc2025
+               MIN(timestamp) as timestamp,
+               last_value(Max("/RealOutputs/RobotState/OdometryPose").x IGNORE nulls) OVER (PARTITION BY filename ORDER BY loop_count ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS PoseX1,
+               last_value(Max("/RealOutputs/RobotState/OdometryPose").y IGNORE nulls) OVER (PARTITION BY filename ORDER BY loop_count ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS PoseY1,
+               last_value(Max("/RealOutputs/RobotState/OdometryPose").value IGNORE nulls) OVER (PARTITION BY filename ORDER BY loop_count ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS PoseTheta1,
+               last_value(Max("/RealOutputs/Odometry/Robot").x IGNORE nulls) OVER (PARTITION BY filename ORDER BY loop_count ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS PoseX2,
+               last_value(Max("/RealOutputs/Odometry/Robot").y IGNORE nulls) OVER (PARTITION BY filename ORDER BY loop_count ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS PoseY2,
+               last_value(Max("/RealOutputs/Odometry/Robot").value IGNORE nulls) OVER (PARTITION BY filename ORDER BY loop_count ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS PoseTheta2,
+               last_value(Max("/RealOutputs/SwerveChassisSpeeds/Measured").vx IGNORE nulls) OVER (PARTITION BY filename ORDER BY loop_count ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS XVelocity,
+               last_value(Max("/RealOutputs/SwerveChassisSpeeds/Measured").vy IGNORE nulls) OVER (PARTITION BY filename ORDER BY loop_count ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS YVelocity,
+               last_value(Max("/RealOutputs/SwerveChassisSpeeds/Measured").omega IGNORE nulls) OVER (PARTITION BY filename ORDER BY loop_count ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS OmegaVelocity,
+               last_value(Max("/DriverStation/Enabled") IGNORE nulls) OVER (PARTITION BY filename ORDER BY loop_count ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS Enabled,
+               last_value(Max("/RealOutputs/Launcher/State") IGNORE nulls) OVER (PARTITION BY filename ORDER BY loop_count ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS IntakeState,
+               last_value(max("/DriverStation/MatchTime") IGNORE nulls) OVER (PARTITION BY filename ORDER BY loop_count ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS MatchTime,
+               last_value(max("/Launcher/Sensor/InnerSensorTriggered") IGNORE nulls) OVER (PARTITION BY filename ORDER BY loop_count ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS InnerSensorTriggered,
+               last_value(max("/DriverStation/MatchNumber") IGNORE nulls) OVER (PARTITION BY filename ORDER BY loop_count ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS MatchNumber,
+               last_value(max("/DriverStation/MatchType") IGNORE nulls) OVER (PARTITION BY filename ORDER BY loop_count ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS MatchType,
+               last_value(max("/DriverStation/EventName") IGNORE nulls) OVER (PARTITION BY filename ORDER BY loop_count ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS EventName, 
+       FROM frc2025
         GROUP BY loop_count, filename
         ORDER BY loop_count)
       WHERE enabled)
@@ -85,12 +91,27 @@ event_alignment AS (
   WHERE IsIntaking
   GROUP BY filename, intake_number
 )
-SELECT LOWER(EventName) AS EventName, UPPER(CONCAT(MATCH_TYPE_STRING(MatchType), CAST(MatchNumber AS INTEGER))) AS Match, RIGHT(CAST(to_seconds(CAST(MatchTime AS INTEGER)) AS VARCHAR),5) AS MatchTime,
-  first_reef_sensor_timestamp - first_feed_rising_edge AS IntakeTime
-  FROM event_alignment
-  WHERE IntakeTime IS NOT NULL AND IntakeTime < 50
-  ORDER BY filename, intake_number;
-""").fetchdf()
+  SELECT LOWER(EventName) AS EventName, UPPER(CONCAT(MATCH_TYPE_STRING(MatchType), CAST(MatchNumber AS INTEGER))) AS Match, RIGHT(CAST(to_seconds(CAST(MatchTime AS INTEGER)) AS VARCHAR),5) AS MatchTime,
+    first_reef_sensor_timestamp - first_feed_rising_edge AS IntakeTime
+    FROM event_alignment
+    WHERE IntakeTime IS NOT NULL AND IntakeTime < 50
+    ORDER BY filename, intake_number;
+  """).fetchdf()
+
+df = get_data()
+
+# Print mean intake time for each event
+for event, group in df[["IntakeTime", "EventName"]].groupby("EventName"):
+    stats = group["IntakeTime"].describe()
+    st.write(f"### {event}")
+    
+    col1, col2, col3 = st.columns(3)
+    col1.metric("Mean", f"{stats['mean']:.2f}s", border=True)
+    col2.metric("25%", f"{stats['25%']:.2f}s", border=True)
+    col3.metric("75%", f"{stats['75%']:.2f}s", border=True)
+    
+    st.divider()
+
 
 st.dataframe(
     df,
@@ -103,3 +124,5 @@ st.dataframe(
     },
     hide_index=True,
 )
+
+st.write(df[["IntakeTime", "EventName"]].groupby("EventName").describe())
--- query/intake_metrics.sql
+++ query/intake_metrics.sql
@@ -1,41 +1,87 @@
+-- Drive Station Lines (Standard Form) Ax + By + C = 0
+SET VARIABLE Blue1A = -0.59;
+SET VARIABLE Blue1B = -0.84;
+SET VARIABLE Blue1C = 1.01;
 
-  WITH datatable AS (
+SET VARIABLE Blue2A = 0.63;
+SET VARIABLE Blue2B = -0.84;
+SET VARIABLE Blue2C = 5.72;
+
+SET VARIABLE Red1A = 0.63;
+SET VARIABLE Red1B = 0.84;
+SET VARIABLE Red1C = -16.71;
+
+SET VARIABLE Red2A = -0.59;
+SET VARIABLE Red2B = 0.84;
+SET VARIABLE Red2C = 9.35;
+
+WITH data_table AS (
+  SELECT *,
+         NOT LAG(IsFeeding) OVER (PARTITION BY filename ORDER BY loop_count) AND IsFeeding AS IsFeedingRisingEdge,
+         NOT LAG(IsIntaking) OVER (PARTITION BY filename ORDER BY loop_count) AND IsFeeding AS IsIntakingRisingEdge,
+         NOT LAG(InnerSensorTriggered) OVER (PARTITION BY filename ORDER BY loop_count) AND InnerSensorTriggered AS InnerSensorTriggeredRisingEdge,
+         NOT LAG(IntakeStoppedWithCoral) OVER (PARTITION BY filename ORDER BY loop_count) AND IntakeStoppedWithCoral AS IntakeStoppedWithCoralRisingEdge,
+  FROM (
     SELECT *,
-          NOT LAG(ReefSensorTriggered) OVER (PARTITION BY filename ORDER BY loop_count) AND ReefSensorTriggered AND LauncherState = 'SHOOTING' AS ReefSensorTriggeredRisingEdge,
-          NOT LAG(LateratorExtended) OVER (PARTITION BY filename ORDER BY loop_count) AND LateratorExtended AS LateratorExtendedRisingEdge
-    FROM (
-      SELECT loop_count, filename,
-          last_value(max(timestamp) IGNORE nulls) OVER (PARTITION BY filename ORDER BY loop_count ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS timestamp,
-          last_value(max("/RealOutputs/Launcher/State") IGNORE nulls) OVER (PARTITION BY filename ORDER BY loop_count ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS LauncherState,
-          last_value(max("/RealOutputs/Elevator/Profile/GoalPosition") IGNORE nulls) OVER (PARTITION BY filename ORDER BY loop_count ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS ElevatorGoalPosition,
-          last_value(max("/RealOutputs/Laterator/Setpoint") IGNORE nulls) OVER (PARTITION BY filename ORDER BY loop_count ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS LateratorSetpoint,
-          last_value(max("/RealOutputs/Launcher/AutoScoreEnabled") IGNORE nulls) OVER (PARTITION BY filename ORDER BY loop_count ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS AutoScoreArmed,
-          last_value(max("/Launcher/Sensor/ReefSensorTriggered") IGNORE nulls) OVER (PARTITION BY filename ORDER BY loop_count ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS ReefSensorTriggered,
-          last_value(max("/DriverStation/MatchTime") IGNORE nulls) OVER (PARTITION BY filename ORDER BY loop_count ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS MatchTime,
-          LateratorSetpoint > 0.25 AS LateratorExtended
-      FROM frc2025
-    GROUP BY loop_count, filename
-    ORDER BY loop_count)
-  ),
-  shooting_sequence AS (
-    SELECT *, 
-          SUM(CASE 
-              WHEN LateratorExtendedRisingEdge THEN 1 
-              ELSE 0 
-          END) OVER (PARTITION BY filename ORDER BY timestamp) AS shot_number
-    FROM datatable WHERE LateratorExtended
-    ORDER BY filename, loop_count
-  ),
-  event_alignment AS (
-    SELECT filename, shot_number,
-          MAX(MatchTime) AS MatchTime,
-          MIN(timestamp) FILTER (WHERE LateratorExtendedRisingEdge) AS first_laterator_timestamp,
-          MIN(timestamp) FILTER (WHERE ReefSensorTriggeredRisingEdge) AS first_reef_sensor_timestamp
-    FROM shooting_sequence
-    GROUP BY filename, shot_number
-  )
-  SELECT filename, shot_number, RIGHT(CAST(to_seconds(CAST(MatchTime AS INTEGER)) AS VARCHAR),5) AS MatchTime, first_laterator_timestamp, first_reef_sensor_timestamp, 
-        first_reef_sensor_timestamp - first_laterator_timestamp AS shot_duration
-  FROM event_alignment
-  WHERE shot_duration IS NOT NULL
-  ORDER BY filename, shot_number;
+      ABS(OmegaVelocity) + ABS(XVelocity) + ABS(YVelocity) < 0.01 AS RobotStopped,
+      CASE WHEN PoseX1 IS NULL THEN PoseX2 ELSE PoseX1 END AS PoseX,
+      CASE WHEN PoseY1 IS NULL THEN PoseX2 ELSE PoseY1 END AS PoseY,
+      CASE WHEN PoseTheta1 IS NULL THEN PoseTheta2 ELSE PoseTheta1 END AS PoseTheta,
+      LINE_TO_POINT(getvariable('Blue1A'), getvariable('Blue1B'), getvariable('Blue1C'), PoseX, PoseY) As DistanceToBlueLeftFeeder,
+      LINE_TO_POINT(getvariable('Blue2A'), getvariable('Blue2B'), getvariable('Blue2C'), PoseX, PoseY) As DistanceToBlueRightFeeder,
+      LINE_TO_POINT(getvariable('Red1A'), getvariable('Red1B'), getvariable('Red1C'), PoseX, PoseY) As DistanceToRedLeftFeeder,
+      LINE_TO_POINT(getvariable('Red2A'), getvariable('Red2B'), getvariable('Red2C'), PoseX, PoseY) As DistanceToRedRightFeeder,
+      LEAST(DistanceToBlueLeftFeeder, DistanceToBlueRightFeeder, DistanceToRedLeftFeeder, DistanceToRedRightFeeder) AS MinDistanceToFeeder,
+      MinDistanceToFeeder < 1.5 AND IntakeState = 'INTAKING' AS IsFeeding,
+      IntakeState LIKE '%INTAK%' AS IsIntaking,
+      IntakeState = 'STOPPED' AND InnerSensorTriggered AS IntakeStoppedWithCoral,
+      FROM (
+        SELECT loop_count, filename,
+               MIN(timestamp) as timestamp,
+               last_value(Max("/RealOutputs/RobotState/OdometryPose").x IGNORE nulls) OVER (PARTITION BY filename ORDER BY loop_count ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS PoseX1,
+               last_value(Max("/RealOutputs/RobotState/OdometryPose").y IGNORE nulls) OVER (PARTITION BY filename ORDER BY loop_count ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS PoseY1,
+               last_value(Max("/RealOutputs/RobotState/OdometryPose").value IGNORE nulls) OVER (PARTITION BY filename ORDER BY loop_count ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS PoseTheta1,
+               last_value(Max("/RealOutputs/Odometry/Robot").x IGNORE nulls) OVER (PARTITION BY filename ORDER BY loop_count ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS PoseX2,
+               last_value(Max("/RealOutputs/Odometry/Robot").y IGNORE nulls) OVER (PARTITION BY filename ORDER BY loop_count ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS PoseY2,
+               last_value(Max("/RealOutputs/Odometry/Robot").value IGNORE nulls) OVER (PARTITION BY filename ORDER BY loop_count ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS PoseTheta2,
+               last_value(Max("/RealOutputs/SwerveChassisSpeeds/Measured").vx IGNORE nulls) OVER (PARTITION BY filename ORDER BY loop_count ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS XVelocity,
+               last_value(Max("/RealOutputs/SwerveChassisSpeeds/Measured").vy IGNORE nulls) OVER (PARTITION BY filename ORDER BY loop_count ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS YVelocity,
+               last_value(Max("/RealOutputs/SwerveChassisSpeeds/Measured").omega IGNORE nulls) OVER (PARTITION BY filename ORDER BY loop_count ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS OmegaVelocity,
+               last_value(Max("/DriverStation/Enabled") IGNORE nulls) OVER (PARTITION BY filename ORDER BY loop_count ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS Enabled,
+               last_value(Max("/RealOutputs/Launcher/State") IGNORE nulls) OVER (PARTITION BY filename ORDER BY loop_count ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS IntakeState,
+               last_value(max("/DriverStation/MatchTime") IGNORE nulls) OVER (PARTITION BY filename ORDER BY loop_count ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS MatchTime,
+               last_value(max("/Launcher/Sensor/InnerSensorTriggered") IGNORE nulls) OVER (PARTITION BY filename ORDER BY loop_count ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS InnerSensorTriggered,
+               last_value(max("/DriverStation/MatchNumber") IGNORE nulls) OVER (PARTITION BY filename ORDER BY loop_count ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS MatchNumber,
+               last_value(max("/DriverStation/MatchType") IGNORE nulls) OVER (PARTITION BY filename ORDER BY loop_count ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS MatchType,
+               last_value(max("/DriverStation/EventName") IGNORE nulls) OVER (PARTITION BY filename ORDER BY loop_count ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS EventName, 
+       FROM frc2025
+        GROUP BY loop_count, filename
+        ORDER BY loop_count)
+      WHERE enabled)
+),
+intaking_sequence AS (
+  SELECT *, 
+         SUM(CASE 
+             WHEN IsFeedingRisingEdge THEN 1 
+             ELSE 0 
+         END) OVER (PARTITION BY filename ORDER BY timestamp) AS intake_number
+  FROM data_table
+  ORDER BY filename, loop_count
+),
+event_alignment AS (
+  SELECT filename, intake_number,
+        MAX(MatchTime) AS MatchTime,
+        MAX(EventName) AS EventName,
+        MAX(MatchType) AS MatchType,
+        MAX(MatchNumber) AS MatchNumber,
+        MIN(timestamp) FILTER (WHERE IsFeedingRisingEdge) AS first_feed_rising_edge,
+        MIN(timestamp) FILTER (WHERE InnerSensorTriggeredRisingEdge) AS first_reef_sensor_timestamp
+  FROM intaking_sequence
+  WHERE IsIntaking
+  GROUP BY filename, intake_number
+)
+  SELECT LOWER(EventName) AS EventName, UPPER(CONCAT(MATCH_TYPE_STRING(MatchType), CAST(MatchNumber AS INTEGER))) AS Match, RIGHT(CAST(to_seconds(CAST(MatchTime AS INTEGER)) AS VARCHAR),5) AS MatchTime,
+    first_reef_sensor_timestamp - first_feed_rising_edge AS IntakeTime
+    FROM event_alignment
+    WHERE IntakeTime IS NOT NULL AND IntakeTime < 50
+    ORDER BY filename, intake_number;
